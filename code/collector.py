104dd2233200250fadb19d596775fdbefa23222b
ChrisCHumphreys <chrishumphreys@protonmail.com>
2026-09-14 21:35:05 -0500

    dotenv working, updtaed gitignore

--- code/collector.py
+++ code/collector.py
@@ -1,11 +1,15 @@
 import requests
 from pprint import pprint
+from dotenv import dotenv_values
 
 # Saving this for reference while I work, but its just a copy and paste of a search URL I ran manually
 test_URL = """https://www.google.com/search?q=Free Streaming&sca_esv=6a464870af6be845&sxsrf=APpeQntsmLYZR-H8-LTAO5HVJjzhOz7sTg:1789164589470&ei=LXykauiwHKvKp84PzZS3oQ8&biw=2493&bih=623&ved=2ahUKEwio-7K-xeeWAxUr5ckDHU3KLfQQ4dUDegQIBhAM&oq=Free Streaming&gs_lp=Egxnd3Mtd2l6LXNlcnAiDkZyZWUgU3RyZWFtaW5nMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMhcQLhjcBhi4BhjaBhjYAhjIAxiwA9gBATIXEC4Y3AYYuAYY2gYY2AIYyAMYsAPYAQEyFxAuGNwGGLgGGNoGGNgCGMgDGLAD2AEBMhcQLhjcBhi4BhjaBhjYAhjIAxiwA9gBATIXEC4Y3AYYuAYY2gYY2AIYyAMYsAPYAQEyFxAuGNwGGLgGGNoGGNgCGMgDGLAD2AEBMhcQLhjcBhi4BhjaBhjYAhjIAxiwA9gBAUjnFFAAWABwAXgBkAEAmAEAoAEAqgEAuAEMyAEAmAIBoAIKmAMAiAYBkAYTugYGCAEQARgZkgcBMaAHALIHALgHAMIHAzItMcgHB4AIAQ&sclient=gws-wiz-serp"""
 
 class Collector:
     """Class to contain collecter functions"""
+    def __init__(self):
+        self.creds = dotenv_values(".env")
+    
     def get_raw_url_content(self, URL):
         """This is a first function to just pull in a single URL"""
     
@@ -21,6 +25,10 @@ class Collector:
         print(f"Response Code: {content.status_code}")
         print(f"Website Text: {content.text}")
 
+    def get_normalized_serp(self, URL):
+        """This will pull back and return a single nomralized page og google results"""
+        print("In Here")
+    
     # for i in range(10):
     #     output_requests_response(test_URL)def get_raw_url_content(URL):
 
