df462ed5a42da2659c9579994e3ffda6884463ed
ChrisCHumphreys <chrishumphreys@protonmail.com>
2026-09-15 21:31:54 -0500

    I am getting results, now I need to find out how to isolate the ads

--- code/collector.py
+++ code/collector.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 from dotenv import dotenv_values
+import json
 
 # Saving this for reference while I work, but its just a copy and paste of a search URL I ran manually
 test_URL = """https://www.google.com/search?q=Free Streaming&sca_esv=6a464870af6be845&sxsrf=APpeQntsmLYZR-H8-LTAO5HVJjzhOz7sTg:1789164589470&ei=LXykauiwHKvKp84PzZS3oQ8&biw=2493&bih=623&ved=2ahUKEwio-7K-xeeWAxUr5ckDHU3KLfQQ4dUDegQIBhAM&oq=Free Streaming&gs_lp=Egxnd3Mtd2l6LXNlcnAiDkZyZWUgU3RyZWFtaW5nMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMhcQLhjcBhi4BhjaBhjYAhjIAxiwA9gBATIXEC4Y3AYYuAYY2gYY2AIYyAMYsAPYAQEyFxAuGNwGGLgGGNoGGNgCGMgDGLAD2AEBMhcQLhjcBhi4BhjaBhjYAhjIAxiwA9gBATIXEC4Y3AYYuAYY2gYY2AIYyAMYsAPYAQEyFxAuGNwGGLgGGNoGGNgCGMgDGLAD2AEBMhcQLhjcBhi4BhjaBhjYAhjIAxiwA9gBAUjnFFAAWABwAXgBkAEAmAEAoAEAqgEAuAEMyAEAmAIBoAIKmAMAiAYBkAYTugYGCAEQARgZkgcBMaAHALIHALgHAMIHAzItMcgHB4AIAQ&sclient=gws-wiz-serp"""
@@ -8,7 +9,7 @@ test_URL = """https://www.google.com/search?q=Free Streaming&sca_esv=6a464870af6
 class Collector:
     """Class to contain collecter functions"""
     def __init__(self):
-        self.creds = dotenv_values(".env")
+        self.creds = dotenv_values('.env')
         self.username = self.creds['OXY_USERNAME']
         self.password = self.creds['OXY_PASS']
     
@@ -27,9 +28,19 @@ class Collector:
         print(f"Response Code: {content.status_code}")
         print(f"Website Text: {content.text}")
 
-    def get_normalized_serp(self, URL):
+    def get_normalized_google_serp(self, query):
         """This will pull back and return a single nomralized page og google results"""
-        print("In Here")
+        payload = {
+            'source' : 'google_ads',
+            'query' : query,
+            'geo_location' : 'Chicago,United States',
+            'parse' : True,
+            'user_agent_type' : 'desktop'
+        }
+
+        response = requests.request('POST', 'https://realtime.oxylabs.io/v1/queries', auth=(self.username, self.password), json=payload)
+        # ads_list = json.dumps(response.json()['results'][0]['content']['results']['paid'])
+        return response
     
     # for i in range(10):
     #     output_requests_response(test_URL)def get_raw_url_content(URL):
