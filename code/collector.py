9154e447e9642711971608fbdc3f989501019f49
ChrisCHumphreys <chrishumphreys@protonmail.com>
2026-09-19 09:57:29 -0500

    threading seems to be working

--- code/collector.py
+++ code/collector.py
@@ -2,6 +2,12 @@ import requests
 from pprint import pprint
 from dotenv import dotenv_values
 import json
+import threading
+
+# Note that much of the threading code was borrowed/inspried from the
+# tutorials at
+# https://reintech.io/blog/how-to-create-a-multi-threaded-application-with-python
+
 
 # Saving this for reference while I work, but its just a copy and paste of a search URL I ran manually
 test_URL = """https://www.google.com/search?q=Free Streaming&sca_esv=6a464870af6be845&sxsrf=APpeQntsmLYZR-H8-LTAO5HVJjzhOz7sTg:1789164589470&ei=LXykauiwHKvKp84PzZS3oQ8&biw=2493&bih=623&ved=2ahUKEwio-7K-xeeWAxUr5ckDHU3KLfQQ4dUDegQIBhAM&oq=Free Streaming&gs_lp=Egxnd3Mtd2l6LXNlcnAiDkZyZWUgU3RyZWFtaW5nMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMhcQLhjcBhi4BhjaBhjYAhjIAxiwA9gBATIXEC4Y3AYYuAYY2gYY2AIYyAMYsAPYAQEyFxAuGNwGGLgGGNoGGNgCGMgDGLAD2AEBMhcQLhjcBhi4BhjaBhjYAhjIAxiwA9gBATIXEC4Y3AYYuAYY2gYY2AIYyAMYsAPYAQEyFxAuGNwGGLgGGNoGGNgCGMgDGLAD2AEBMhcQLhjcBhi4BhjaBhjYAhjIAxiwA9gBAUjnFFAAWABwAXgBkAEAmAEAoAEAqgEAuAEMyAEAmAIBoAIKmAMAiAYBkAYTugYGCAEQARgZkgcBMaAHALIHALgHAMIHAzItMcgHB4AIAQ&sclient=gws-wiz-serp"""
@@ -12,6 +18,7 @@ class Collector:
         self.creds = dotenv_values('.env')
         self.username = self.creds['OXY_USERNAME']
         self.password = self.creds['OXY_PASS']
+        self.ads_list = []
     
     def get_raw_url_content(self, URL):
         """This is a first function to just pull in a single URL"""
@@ -29,7 +36,9 @@ class Collector:
         print(f"Website Text: {content.text}")
 
     def get_normalized_google_serp(self, query):
-        """This will pull back and return a single nomralized page og google results"""
+        """This will pull back and return a single nomralized page of google
+        results"""
+
         payload = {
             'source' : 'google_ads',
             'query' : query,
@@ -38,23 +47,57 @@ class Collector:
             'user_agent_type' : 'desktop'
         }
 
-        response = requests.request('POST', 'https://realtime.oxylabs.io/v1/queries', auth=(self.username, self.password), json=payload)
-        # ads_list = json.dumps(response.json()['results'][0]['content']['results']['paid'])
+        response = requests.request(
+            'POST', 'https://realtime.oxylabs.io/v1/queries',
+            auth=(self.username, self.password), json=payload)
+        # ads_list = json.dumps(
+        #     response.json()['results'][0]['content']['results']['paid'])
         return response
 
     def extract_ads_from_json_response(self, serp_response):
-        """This function will return just the ads from a raw JSON return from the SERP"""
+        """This function will return just the ads from a raw
+        JSON return from the SERP"""
 
         return serp_response.json()['results'][0]['content']['results']['paid']
 
     def get_adds_from_query(self, query):
-        """Funciton that wraps together smaller functions to take in a query and return just the ad section of the url"""
+        """Funciton that wraps together smaller functions to take in a
+        query and return just the ad section of the url"""
 
         response = self.get_normalized_google_serp(query)
         ads = self.extract_ads_from_json_response(response)
 
-        return ads
+        self.ads_list.append(ads)
         
+        return ads
+
+    def pull_ads_with_threading(self, query, count):
+        """This is to allow for multiple threads to run at once
+           Code here was adapted and borrowed from
+           https://reintech.io/blog/how-to-create-a-multi-threaded-application-with-python
+           and https://docs.python.org/3/library/threading.html
+        """
+
+        # For testing putting this at 3, but likely will go to 10 for real use
+        threading.Semaphore(3)
+
+        threads = []
+        for i in range(count):
+            thread = threading.Thread(
+                target=self.get_adds_from_query,
+                args=(query,)
+            )
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        print("Testing Ran executed!")
+
+        
+
+    
     
     # for i in range(10):
     #     output_requests_response(test_URL)def get_raw_url_content(URL):
